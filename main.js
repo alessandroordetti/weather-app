f3640d3f8e9da34069178b3f2fc6dbe087a3960c
Alessandro Ordetti <Ale.Ordetti@unipegaso.local>
2022-11-17 10:07:46 +0100

    No image found function added

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
 }); */
 
 
+
 axios.get('https://api.openweathermap.org/data/2.5/weather?lat=40.853294&lon=14.305573&appid=779225edbe9ab6777587f2b32f56db93&units=metric')
 .then(response=>{
     console.log(response.data);
